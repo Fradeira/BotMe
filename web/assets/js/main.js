a7bf7673af5e4cca317c73553d46ee36e6242838
Fradeira Guillaume <guillaumefradeira@yahoo.fr>
2018-04-11 01:14:12 +0200

    Correctif + Ajout Menu Principal

--- web/assets/js/main.js
+++ web/assets/js/main.js
@@ -4,6 +4,31 @@ $(document).ready(function () {
         e.preventDefault();
 
         $("#connect").toggle("slow");
+        $("#signup").hide();
+        $("#mdpmiss").hide();
+    });
+
+    $('.signup').on('click', function (e) {
+        e.preventDefault();
+
+        $("#signup").toggle("slow");
+        $("#connect").hide();
+        $("#mdpmiss").hide();
+    });
+
+    $('#sign').on('click', function (e) {
+        e.preventDefault();
+
+        $("#connect").hide();
+        $("#signup").toggle("slow");
+    });
+
+    $('#passmiss').on('click', function (e) {
+        e.preventDefault();
+
+        $("#signup").hide();
+        $("#connect").hide();
+        $("#mdpmiss").toggle("slow");
     });
 });
 
