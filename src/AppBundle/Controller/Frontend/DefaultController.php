3ace780db8e0629ee3cf59ba8df017e5b9eb2cab
Kevin <morand.kevin55@gmail.com>
2018-04-16 00:15:18 +0200

    Réorganisation de la structure plus mise en place du backoffice

--- src/AppBundle/Controller/Frontend/DefaultController.php
+++ src/AppBundle/Controller/Frontend/DefaultController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace AppBundle\Controller;
+namespace AppBundle\Controller\Frontend;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -13,8 +13,8 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        return $this->render('front/default/index.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR."/app/views/front",
         ]);
     }
 
@@ -23,7 +23,7 @@ class DefaultController extends Controller
      */
     public function basicConsoleAction(Request $request)
     {
-        return $this->render('default/botConsole/basicConsole.html.twig', [
+        return $this->render('front/default/botConsole/basicConsole.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
     }
